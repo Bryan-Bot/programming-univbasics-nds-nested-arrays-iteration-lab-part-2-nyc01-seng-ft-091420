c97de78ee6ac6c379851b5aeae3ea75a00e01883
Bryan D. Gonzalez <bryan.dujari@gmail.com>
2020-08-25 17:08:31 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -8,11 +8,12 @@ def find_min_in_nested_arrays(src)
       count_2 = 0
       while count_2 < src[count].length do
 
-        if (src[count][count_2].min)
+        if (src[count][count_2].length < new_Array.length )
           new_Array.push(src[count][count_2])
         end
       count_2 += 1
       end
       count += 1
     end
+    new_Array
 end
